dd4a761ebdcc35874bb674366105fa2031e6d2b2
darin.m <darin.m@tvzavr.ru>
2018-08-16 12:42:54 +0300

    func form

--- funti.go
+++ funti.go
@@ -6,3 +6,23 @@ package main
 import(
 	"fmt"
 )
+
+//
+// Canonical form function declaration
+//
+// the 'func' keyword marks the beginning of a function declaration
+// <func-id> an optional identifier that is used to name the function for future reference
+// <arg-list> a required set of parentheses enclosing an optional list of fucntion arguments
+// <result-list> an optional list of types for values returned by the function.
+// when function returns more than one value, an enclosing set of parentheses is ruquired
+
+// func [<func-id>] ([<arg-list>]) [(<result-list>)] {
+// [return] [<value or expression list>]
+//}
+
+// the 'return' statement causes the execution flow to exit function.
+// when the function defines returned values in its definition, a return statement is 
+// required as the last logical statement of the function. 
+// Otherwise, if no return values arespecified, the return statement is optional.
+
+
