9105b8785a4b0e1d9d956a7641e219f4783cf056
mdarin <m.darin.comco@yandex.ru>
2018-08-19 20:55:24 +0300

    defer

--- defca.go
+++ defca.go
@@ -1,7 +1,31 @@
 //
 // Deferring function calls
 //
+package main
+
+import (
+	"fmt"
+)
 
 // Go supports the notion of deferring a function call. Placing the keyword defer before a
 // function call has the interesting effect of pushing the function unto an internal stack,
 // delaying its execution right before the enclosing function returns.
+
+func do(steps ...string) {
+	defer fmt.Println("* All done!")
+	for _, s := range steps {
+		defer fmt.Println(" -",s)
+	}
+	fmt.Println("* Starting")
+}
+
+func main() {
+	var steps = []string{
+		"Find key",
+		"Aplly break",
+		"Put key in ignition",
+		"Start car",
+	}
+
+	do(steps...)
+}
