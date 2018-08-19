ecbf650c0611131bedd91f60f767e2b9f6577569
mdarin <m.darin.comco@yandex.ru>
2018-08-19 21:11:33 +0300

    defer

--- defca.go
+++ defca.go
@@ -5,6 +5,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 // Go supports the notion of deferring a function call. Placing the keyword defer before a
@@ -28,4 +29,25 @@ func main() {
 	}
 
 	do(steps...)
+
+	//
+	// Using defer
+	//
+	// The defer keyword modifies the execution flow of a program by delaying function calls.
+	// One idiomatic usage for this feature is to do a resource cleanup.
+	readFile("")
+}
+
+func readFile(fname string) ([]string, error) {
+	//...
+	file, err := os.Open(fname)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	//...
+	return []string{""}, nil
+	// The pattern of opening-defer-closing resources is widely used in Go. By placing the
+	// deferred intent immediately after opening or creating a resource allows the code to read
+	// naturally and reduces the likeliness of creating a resource leakage.
 }
